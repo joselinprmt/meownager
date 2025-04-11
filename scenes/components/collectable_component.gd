ab9b27ec875f8679bdeb5b9702b999e4254272d4
Joselin Permata Aprillia <103546691+joselinprmt@users.noreply.github.com>
2025-04-11 19:18:16 +0700

    feat(main): added sfx and back button

--- scenes/components/collectable_component.gd
+++ scenes/components/collectable_component.gd
@@ -3,9 +3,12 @@ extends Area2D
 
 @export var collectable_name: String
 
+@onready var sfx_collect: AudioStreamPlayer2D = $sfxCollect
+
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
+		sfx_collect.play()
 		InventoryManager.add_collectable(collectable_name)
-		#print("Collected: ", collectable_name)
+		await get_tree().create_timer(0.15).timeout
 		get_parent().queue_free()
--- scripts/globals/game_manager.gd
+++ scripts/globals/game_manager.gd
@@ -1,8 +1,14 @@
 extends Node
 
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("back_menu"):
+		get_tree().change_scene_to_file("res://scenes/ui/menu_screen.tscn")
+
+
 func start_game() -> void:
 	pass
+
 
 func exit_grame() -> void:
 	get_tree().quit()
